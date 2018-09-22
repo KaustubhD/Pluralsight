cc5044ef1080fa4c7770bdb7c9b95b3cd63e4f05
KaustubhD <kaustubhd287@gmail.com>
2018-09-22 11:02:59 +0530

    Module 6

--- Angular-Getting Started/src/app/products/product-list.service.ts
+++ Angular-Getting Started/src/app/products/product-list.service.ts
@@ -23,6 +23,7 @@ export class ProductListService{
         "productName": "Garden Cart",
         "productCode": "GDN-0023",
         "releaseDate": "March 18, 2016",
+        "description": "15 gallon capacity rolling garden cart",
         "price": 32.99,
         "starRating": 4.2,
         "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
