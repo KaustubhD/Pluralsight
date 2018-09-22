7f2dbaaad7ba11f5bbbe3957ee21fd3d0c643c96
KaustubhD <kaustubhd287@gmail.com>
2018-09-22 16:54:22 +0530

    Added detail component

--- Angular-Getting Started/src/app/app.module.ts
+++ Angular-Getting Started/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ProductListComponent } from './products/product-list.component';
 import { AddSpaces } from './shared/addSpaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
     ProductListComponent,
     AddSpaces,
     StarComponent,
+    ProductDetailComponent,
   ],
   imports: [
     BrowserModule,
